b3fde872581509f78c03dec359ffddfcaddb6546
Qiqa Q Putuzo <qnputuzo@gmail.com>
2022-12-08 05:50:24 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *  Main entry point
+ *
+ *  Retun : always return 0(success)
+ */
+
+int main(void)
+{
+	/**
+	 * This is a short desprtion of printf
+	 *
+	 */
+
+	printf("with proper grammar, but the outcome is a piece of art,\N");
+	/**
+	 * This is short disceiption of return
+	 *
+	 *
+	 */
+
+	return (0);
+
+}
