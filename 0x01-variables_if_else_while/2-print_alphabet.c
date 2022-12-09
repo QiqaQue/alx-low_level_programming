83826ebfac23dbf35e785957982a6edd98bfb464
Qiqa Q Putuzo <qnputuzo@gmail.com>
2022-12-09 09:09:27 -0800

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - blsh blah blag
+ *
+ * Return: blah blha hax jxnwd
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (char a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+
+}
