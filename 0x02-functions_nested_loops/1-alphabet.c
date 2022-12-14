de26c47285cafa8cea217d390c95e73bfdf1885f
Qiqa Q Putuzo <qnputuzo@gmail.com>
2022-12-14 11:33:20 -0800

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet- Prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: void
+ */
+
+/*comment for the sake of a com*/
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a < 'z'; a++)
+		_putchar(a);
+
+	_putchar('\n');
+
+}
