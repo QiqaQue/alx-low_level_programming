bfb10a3955fa1984fb090f4c2ffc52cd547370b4
Qiqa Q Putuzo <qnputuzo@gmail.com>
2022-12-15 09:32:19 -0800

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - that checks for a digit (0 through 9)
+ *
+ * @c: digit
+ *
+ * Return: Returns 1 if c is a digit, Returns 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >='0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
