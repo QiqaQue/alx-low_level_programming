2a719ced838020bf8d5fe72a8c5dc121828d759a
Qiqa Q Putuzo <qnputuzo@gmail.com>
2022-12-21 10:29:42 -0800

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen -  returns the length of a string
+ *
+ * @s: string
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
